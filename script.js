faa34dd0b082ed5081eab19c7599be1a3fe64295
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-11-13 12:01:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,56 @@ function generatePassword() {
         charsetPassword = 'abcdefghijklmnopqrstuvwxyz';
         charsetPassword += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         charsetPassword += '0123456789';
-        charsetPassword += '~`!@#$%^&*()_-+';
-        
+        //charsetPassword += '~`!@#$%^&*()_-+';
+
     for(let i = 0, n = charsetPassword.length; i < lengthPassword; ++i) {
         retVal += charsetPassword.charAt(Math.floor(Math.random() * n));
     }
     return retVal;
 }
 
-console.log(generatePassword());
+const generatePasswordInput = document.querySelector('#generatePasswordInput');
+        generatePasswordInput.value = generatePassword();
+
+const btnCopyPassword = document.querySelector('#btnCopyPassword');
+
+btnCopyPassword.addEventListener('click', () => {
+    generatePasswordInput.select();
+    generatePasswordInput.setSelectionRange(0, 99999); 
+    /** Свойство Clipboard интерфейса writeText() 
+        записывает указанную текстовую строку в системный буфер обмена.*/
+    navigator.clipboard.writeText(generatePasswordInput.value);
+})
+
+const btnReloadPassword = document.querySelector('#btnReloadPassword');
+btnReloadPassword.addEventListener('click', () => {
+    window.location.reload();
+})
+
+const btnPastPassword = document.querySelector('#btnPastPassword');
+
+btnPastPassword.addEventListener('click', (event) => {
+    let textareaPassword = document.querySelector('#textareaPassword');
+    if(navigator.clipboard){
+        //Вставить текст из буфера обмена в поле ввода textarea
+        navigator.clipboard.readText().then(function(copyText){
+            textareaPassword.innerHTML += copyText+'\n';
+        });
+    } else alert('Ваш браузер не поддерживает Clipboard');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
